806b7c9eca8c66e20f395692d966bcc79a07eb9e
DoKhacDe <82132024+DoKhacDe@users.noreply.github.com>
2021-06-17 18:50:20 +0700

    remove table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call(ActorSeeder::class);
+        $this->call(CountrySeeder::class);
+        $this->call(CompanySeeder::class);
+        $this->call(EpisodeSeeder::class);
+        $this->call(FavouriteSeeder::class);
+        $this->call(GenreSeeder::class);
+        $this->call(MovieSeeder::class);
+        $this->call(PosterSeeder::class);
+        $this->call(UserSeeder::class);
     }
 }
